4ecc51e38c5d9731a8b8689e4f680945afdb7336
andrewariley87 <andrewariley87@gmail.com>
2020-09-10 12:10:09 -0400

    Adds Read Endpoints
    Adds index and show for users and tasks.

--- src/index.js
+++ src/index.js
@@ -15,11 +15,43 @@ app.post('/users', (req, res) => {
       .catch(e => res.status(422).send(e));
 });
 
+app.get('/users', (req, res) => {
+  User.find({})
+      .then(users => res.send(users))
+      .catch(e => res.status(500));
+});
+
+app.get('/users/:id', (req, res) => {
+  User.findById(req.params.id)
+      .then((user) => {
+        if(!user) {
+          return res.status(404).send()
+        }
+        res.send(user)
+      }).catch(e => res.status(500).send(e))
+});
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task.save()
       .then(() => res.status(201).send(task))
       .catch(e => res.status(422).send(e));
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+      .then((tasks) => res.send(tasks))
+      .catch(e => res.status(500));
+});
+
+app.get('/tasks/:id', (req, res) => {
+  Task.findById(req.params.id)
+      .then((task) => {
+        if (!task) {
+          return res.status(404).send();
+        }
+        res.send(task)
+      }).catch((e) => res.status(500).send(e))
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
