811e41838ea6ab1a19840011957eac958ad48fb3
andrewariley87 <andrewariley87@gmail.com>
2020-10-02 11:35:02 -0400

    Logging In Users
    Adds endpoint for validating user credentials.
    Also adds a uniqueness validation for email on user model.

--- src/models/user.js
+++ src/models/user.js
@@ -18,6 +18,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -38,6 +39,22 @@ const userSchema = new mongoose.Schema({
     }
   }
 });
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
 
 userSchema.pre('save', async function (next) {
   const user = this;
