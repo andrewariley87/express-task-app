56d0d33ed8a5c9ba2d5ed8c19f5da31ea9ab664b
andrewariley87 <andrewariley87@gmail.com>
2020-10-08 12:29:54 -0400

    Add Avatar to a User
    Adds the ability to add an avatar to a user.
    Also adds route for a user to delete their avatar.

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,7 +4,6 @@ const User = require('../models/user');
 const auth = require('../middleware/auth');
 const multer = require('multer');
 const avatar = multer({
-  dest: 'avatars',
   limits: {
     fileSize: 1000000
   },
@@ -88,10 +87,18 @@ router.delete('/users/me', auth, async(req, res) => {
   }
 });
 
-router.post('/users/me/avatar', avatar.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', auth, avatar.single('avatar'), async(req, res) => {
+  req.user.avatar = req.file.buffer;
+  await req.user.save();
   res.send();
 }, (error, req, res, next) => {
   res.status(400).send({ error: error.message });
+});
+
+router.delete('/users/me/avatar', auth, async(req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
 });
 
 module.exports = router
